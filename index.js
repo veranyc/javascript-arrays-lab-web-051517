8f6a35bb110848543073b4a5795a45f0f0c2f8b3
veranyc <veranyc@gmail.com>
2017-06-26 22:31:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+let kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var cats = [...kittens, name]
+  return cats
+}
+
+function prependKitten(name) {
+  var cats = [name, ...kittens]
+  return cats
+}
+
+function removeLastKitten() {
+  let cats = kittens.slice(0, kittens.length -1)
+  return cats
+}
+
+function removeFirstKitten() {
+  let cats = kittens.slice(1)
+  return cats
+}
